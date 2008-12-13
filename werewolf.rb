17d9e6da8c5405d7e9f53cf4097324171ad4e8ab
Juanjo Bazan <jjbazan@gmail.com>
2008-12-13 19:07:02 +0100

    action new link

--- werewolf.rb
+++ werewolf.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sequel'
 require 'lib/markaby_for_sinatra'
+require 'db/link'
 
 get '/stylesheets/:filename.css' do
   content_type 'text/css', :charset => 'utf-8'
@@ -11,4 +12,9 @@ end
 
 get '/' do
   markaby :index
+end
+
+get '/links/new' do
+  @link = Link.new
+  markaby :new
 end
