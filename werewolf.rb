a1d0aec863b7876b032b0d586ab3d7531851e74f
Marcelino Llano <info@marze.es>
2008-12-13 18:11:35 +0100

    add css through sass

--- werewolf.rb
+++ werewolf.rb
@@ -4,7 +4,11 @@ require 'sinatra'
 require 'sequel'
 require 'markaby_for_sinatra'
 
+get '/stylesheets/:filename.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass params[:filename].to_sym
+end
+
 get '/' do
-  sass :screen
   markaby :index
 end
